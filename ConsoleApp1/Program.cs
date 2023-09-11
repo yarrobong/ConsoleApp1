3a4bec85ffa45819f4000ce46091160b055bcbdf
yarrobong <oneboardsholi@gmail.com>
2023-09-11 21:49:55 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿int newInt = 10;
+float newFloat = (float) 8.4;
+string newStr = "true";
+
+float bigInt = (float)(newInt * 3.5);
+
+newFloat = newFloat - 1;
+
+Console.WriteLine(newInt / newFloat);
+Console.WriteLine(bigInt / newFloat);
+
+newStr = "NoNo"  + "YesYesYes";
+
+Console.WriteLine("newInt: " + newInt);
+Console.WriteLine("newFloat: " + newFloat);
+Console.WriteLine("newStr: " + newStr);
+
